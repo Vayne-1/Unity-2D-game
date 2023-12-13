f825ef5bd912dd383d3c22d97105848396f30f19
AhmedMedhat123 <117312363+AhmedMedhat123@users.noreply.github.com>
2023-12-14 00:15:45 +0200

    ahmed commit

--- whispers of magic/Assets/code/PlayerStats.cs
+++ whispers of magic/Assets/code/PlayerStats.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class PlayerStats : MonoBehaviour
 {
-    public int health = 6;
-  
-    public int lives = 3;
+    public int health = 100;
+    public int lives = 2;
     public float flickerDura1on = 0.1f;
     private float flickerTime = 0f;
     private SpriteRenderer spriteRenderer;
@@ -13,10 +14,19 @@ public class PlayerStats : MonoBehaviour
     public float immunityDura1on = 1.5f; //el w2t ely sonic 3mal ynor w ytfy
     private float immunityTime = 0f;
 
+    private Animator animator;
+    Rigidbody2D rb2d;
+
+    public Image healthBar;
+    public Image heartBar;
+
     // public int coinsCollected = 0;
     void Start()
     {
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        //StartCoroutine(DestroyAfterDelay());
+        rb2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
@@ -32,35 +42,48 @@ public class PlayerStats : MonoBehaviour
             }
         }
     }
-   
+
     public void TakeDamage(int damage)
     {
         if (this.isImmune == false)
         {
             this.health = this.health - damage;
+            healthBar.fillAmount = (float)this.health / 100f;
+
             if (this.health < 0f)
                 this.health = 0;
+
             if (this.lives > 0f && this.health == 0f)
             {
                 FindObjectOfType<LevelManager>().RespawnPlayer();
-                this.health = 6;
+                this.health = 100;
                 this.lives--;
+                heartBar.fillAmount -= 0.1f;
+                ResetFillAmount();
             }
             else if (this.lives == 0 && this.health == 0)
             {
+                heartBar.fillAmount = 0f;
                 Debug.Log("Gameover"); //add game over splash screen
-                Destroy(this.gameObject);
+                animator.SetTrigger("isDead");
+                if (rb2d != null)
+                {
+                    rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+                }
+                Destroy(gameObject, 4.0f);
             }
             Debug.Log("Player Health : " + this.health.ToString());
             Debug.Log("Player Lives : " + this.lives.ToString());
         }
         PlayHitReac1on();
     }
+
     void PlayHitReac1on()
     {
         this.isImmune = true;
         this.immunityTime = 0f;
     }
+
     void SpriteFlicker()
     {
         if (this.flickerTime < this.flickerDura1on)
@@ -73,10 +96,16 @@ public class PlayerStats : MonoBehaviour
             this.flickerTime = 0;
         }
     }
-    //public void CollectCoin(int coinValue)
-     //{
-        
-    // this.coinsCollected = this.coinsCollected + coinValue;
-        
-   // }
-} //Class
+
+    void ResetFillAmount()
+    {
+        if (healthBar != null)
+        {
+            healthBar.fillAmount = 1f;  // Set to 1 for maximum fill
+        }
+        else
+        {
+            Debug.LogWarning("Image component not assigned.");
+        }
+    }
+} 
